60d60a85696a87a0e7ecd6f0cd72c26d0d6abd93
Pradhuman Singh Chaudhary <113369897+pradhuman09@users.noreply.github.com>
2024-02-29 09:29:08 +0000

    Datatypes Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" ; // treat all JS as newer version 
+
+// alert(3+3) // we are using nodejs , not browser 
+
+console.log(5+4)
+
+console.log("Pradhuman")
+
+let name = "honey"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
